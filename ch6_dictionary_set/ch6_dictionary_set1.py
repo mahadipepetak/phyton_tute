de359f4a379d1416f1e6694240b68bebc7a8f857
mahdi <mahadipepetak@outlook.com>
2021-07-20 00:27:57 +0800

    finished assignment dictionary

--- ch6_dictionary_set/ch6_dictionary_set1.py
+++ ch6_dictionary_set/ch6_dictionary_set1.py
@@ -5,8 +5,57 @@ Created on Mon Jul 19 23:51:26 2021
 @author: mahad
 """
 
-countryList = ["1", "2", "2"]
-countrySet = set(countryList)
+# import library
+import datetime
 
-print(countryList)
-print(countrySet)
+"""
+# PREVIOUS INFO
+songName = "Ragaman"
+singer = "Faizal Tahir"
+dateReleased = datetime.datetime(2018, 8, 24)
+genre = ["reggae", "funk-pop"]
+genreStr = ','.join([str(item) for item in genre])
+
+company = ["Faithful Music", "Warner Music Malaysia"]
+companyStr = ','.join([str(item) for item in genre])
+                      
+writer = ["Faizal Tahir", "Mike Chan", "Romeo", "Karim Karam", "Jaare"]
+writerStr = ','.join([str(item) for item in writer])
+
+duration = "00:04:10" 
+
+"""
+
+# def dectionary
+favSong = {
+    "Name" :"Ragaman",
+    "Singer" : "Faizal Tahir",
+    "Date Released" : datetime.datetime(2018, 8, 24),
+    "Genre" : ["reggae", "funk-pop"],
+    "Company" : ["Faithful Music", "Warner Music Malaysia"],
+    "Writer" : ["Faizal Tahir", "Mike Chan", "Romeo", "Karim Karam", "Jaare"],
+    "Duration" : "00:04:10"
+    }
+
+# define functions
+def printSongMeta(songDict):
+    for key in songDict:
+        text = key + " - " + str(songDict[key])
+        print(text)
+
+def guessKeyVal(key, val):
+    if(key in favSong and favSong[key]==val):
+        print("YESS")
+    else:
+        print("NOO")
+        
+
+# Output info
+# print(favSong)
+printSongMeta(favSong)
+                
+guessKeyVal("Name", "Ragaman") 
+guessKeyVal("Name", "Igaman")   
+guessKeyVal("Hello", "Igaman")   
+        
+        
